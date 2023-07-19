192997a90d3edb00428fd60c38b4529a32b2dc3a
yiren <crystalee.renyi09@gmail.com>
2023-07-18 20:21:41 -0700

    build 1

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/peggie2023.github.io/'
 })
